2d84c3767c5272fb1247a021e571eec4ad623d78
Jesse Machado <jesse.machado@bradesco.com.br>
2025-12-15 15:22:34 -0300

    Add Comments class with initial methods and documentation

---
+++ 0x04/cleanCode/Comments.java
@@ -0,0 +1,22 @@
+public class Comments {
+
+    // TODO Este método deve ser removido
+    public void check(){
+        return;
+    }
+
+    /**
+     * <p> este método publica resultados com base na data
+     * </p>
+     * @param data data a partir da qual os resultados devem ser publicados
+     * @retornar Lista de entidades de resultado
+     * @desde 16.0
+     */
+    public List<Result> publishResults(Date date) {
+        // alguma lógica de negócios aqui
+    }
+
+    public Owner getTestcaseOwner(Testcase testcase){} 
+
+    public Hours getHoursInQuarter(){} 
+}
