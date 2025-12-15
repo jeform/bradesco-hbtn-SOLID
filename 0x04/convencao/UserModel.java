b3dda0d43c7c3896f67cc5c79161599f42b1de56
Jesse Machado <jesse.machado@bradesco.com.br>
2025-12-15 15:29:57 -0300

    Add UserModel class with user entity representation and initial methods

---
+++ 0x04/convencao/UserModel.java
@@ -0,0 +1,14 @@
+// classe user, representando nossa entidade
+
+class UserModel {
+
+    int idUser;
+
+    String descricao;
+
+    final static int IDADE_MINIMA = 18;
+
+    // função que coleta todos os users
+    public getUsers() { return [];}
+
+}
