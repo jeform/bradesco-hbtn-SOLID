601dc58f51a9639212d95f05aeab73cf6279868f
Jesse Machado <jesse.machado@bradesco.com.br>
2025-12-15 15:25:30 -0300

    Remove TODO comment and clean up publishResults method

--- 0x04/cleanCode/Comments.java
+++ 0x04/cleanCode/Comments.java
@@ -1,12 +1,10 @@
 public class Comments {
 
-    // TODO Este método deve ser removido
     public void check(){
         return;
     }
 
     public List<Result> publishResults(Date date) {
-        // alguma lógica de negócios aqui
     }
 
     public Owner getTestcaseOwner(Testcase testcase){} 
